056a2788894e179b12ec7bb87a40e17af529fe16
Tom Castagnino <tcastagnino@gmail.com>
2021-09-19 07:59:42 -0700

    Models created. Task model added to admin

--- ground_control/models.py
+++ ground_control/models.py
@@ -1,3 +1,18 @@
 from django.db import models
+from django.db.models.deletion import PROTECT
+from django.utils import timezone
 
 # Create your models here.
+class Satellite(models.Model):
+    name = models.CharField(max_length=100, primary_key=True)
+
+
+class Task(models.Model):
+    name = models.CharField(max_length=100)
+    resources = models.TextField()
+    date_added = models.DateTimeField(default=timezone.now)
+    # assigned_to = models.ForeignKey(Satellite, on_delete=PROTECT)
+    assigned_to = models.CharField(max_length=100) #TODO: use foreingKey
+
+    def __str__(self):
+        return self.name
