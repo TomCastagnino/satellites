db1bc56290b17014f5e6f000d3c2c0d687243b6d
Tom Castagnino <tcastagnino@gmail.com>
2021-09-21 12:00:32 -0700

    Comments to util methods.

--- ground_control/consumers/earth_utils.py
+++ ground_control/consumers/earth_utils.py
@@ -1,8 +1,15 @@
 
 def sort_task_list(task_list):
+    """
+    :param task_list: each element of the list must include a 'resources' and a 'name' key 
+    """
     return sorted(task_list, key=lambda task: (task['pay_off']/len(task['resources'])), reverse=True)
 
 def distribute_tasks(list_of_tasks):
+    """
+    Takes a list of task and returns a satellite dict and a new list.
+    :param list_of_task: each element of the list must include a 'resources' and a 'name' key
+    """
     satellite = {'tasks': [], 'resources': set()}
     filtered_list = []
     for task in list_of_tasks:
